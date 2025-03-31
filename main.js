2501ec16d1ee3aff1e7b51782e2dd728a4a0ed3b
mao <1829504577@qq.com>
2025-03-31 16:24:09 +0800

    mao
    9

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('node:path');
-const SerialPort = require('serialport'); // 确保导入SerialPort
+const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
-  mainWindow.loadFile('index.html'); 
- 
-
-  const port = new SerialPort('COM5', { baudRate: 9600 }); // 修改为你的串口
-
-  port.on('data', (data) => {
-    mainWindow.webContents.send('serial-data', data.toString());
-  });
+    win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
